52c372c07aeebe3c905dd59925a9a53cb38bcf88
Sersh15121994 <scruz18@alumnos.uaq.mx>
2022-04-13 14:35:57 -0500

    CAMBIOS DE CÓDIGOS

--- gcc/startup_gcc.d
+++
@@ -1,9 +0,0 @@
-gcc/startup_gcc.o: startup_gcc.c \
- c\:\msys64\mingw64\lib\gcc\arm-none-eabi\10.1.0\include\stdint.h \
- c\:\msys64\mingw64\arm-none-eabi\include\stdint.h \
- c\:\msys64\mingw64\arm-none-eabi\include\machine\_default_types.h \
- c\:\msys64\mingw64\arm-none-eabi\include\sys\features.h \
- c\:\msys64\mingw64\arm-none-eabi\include\_newlib_version.h \
- c\:\msys64\mingw64\arm-none-eabi\include\sys\_intsup.h \
- c\:\msys64\mingw64\arm-none-eabi\include\sys\_stdint.h lib/hw_nvic.h \
- lib/hw_types.h
